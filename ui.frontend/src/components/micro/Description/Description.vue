b6372f2a8d0c83f9e885cfb6a4380fc2d6ad9664
Leonardo Luis Klein <leoonardoluis.klein@outlook.com>
2022-08-05 15:30:26 -0300

    Feat: adding description component with basic style

---
+++ ui.frontend/src/components/micro/Description/Description.vue
@@ -0,0 +1,19 @@
+<template>
+    <h3 id="description">{{ description }}</h3>
+</template>
+
+<script>
+export default {
+  name: 'Description',
+  props: {
+    description: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "./Description.scss";
+</style>
