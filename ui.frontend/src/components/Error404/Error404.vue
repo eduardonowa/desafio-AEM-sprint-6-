16a4ca40ba5a7768fe221f589af7ac3de448e051
Leonardo Luis Klein <leoonardoluis.klein@outlook.com>
2022-08-05 16:24:56 -0300

    Feat: error404 component with basic style

---
+++ ui.frontend/src/components/Error404/Error404.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="errorPage">
+        <img src="@/assets/error404.gif" alt="Erro 404" id="errorGif">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Error404'
+}
+</script>
+
+<style lang="scss" scoped>
+@import './Error404.scss';
+</style>
