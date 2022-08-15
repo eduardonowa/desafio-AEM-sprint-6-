865ad91164f910f2a7e45378cfc91bc1cb93ad75
Leonardo Luis Klein <leoonardoluis.klein@outlook.com>
2022-08-14 22:49:50 -0300

    Feat: added vue router in the project

---
+++ core/src/main/java/com/vue/core/models/impl/Error404RedImpl.java
@@ -0,0 +1,49 @@
+/*
+ * ***********************************************************************
+ * SPA Vue Project CONFIDENTIAL
+ * ___________________
+ *
+ * Copyright 2022 SPA Vue Project.
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property
+ * of SPA Vue Project and its suppliers, if any. The intellectual and
+ * technical concepts contained herein are proprietary to SPA Vue Project
+ * and its suppliers and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from SPA Vue Project.
+ * ***********************************************************************
+ */
+
+package com.vue.core.models.impl;
+
+import com.adobe.cq.export.json.ComponentExporter;
+import com.adobe.cq.export.json.ExporterConstants;
+import com.vue.core.models.Error404Red;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+
+@Model(adaptables = {
+    SlingHttpServletRequest.class
+}, adapters = {
+    Error404Red.class,
+    ComponentExporter.class
+}, resourceType = "vue/components/error404-red")
+@Exporter(name = ExporterConstants.SLING_MODEL_EXPORTER_NAME, extensions = ExporterConstants.SLING_MODEL_EXTENSION)
+public class Error404RedImpl
+    implements Error404Red
+{
+
+    @SlingObject
+    private Resource resource;
+
+    @Override
+    public String getExportedType() {
+        return resource.getResourceType();
+    }
+
+}
