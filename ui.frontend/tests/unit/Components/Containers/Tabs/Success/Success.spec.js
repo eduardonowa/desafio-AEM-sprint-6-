413823e3c60d9b7f8f9447b261f313bad11116d3
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-08-18 12:07:59 -0300

    feat: adding new test to success component

--- ui.frontend/tests/unit/Components/Containers/Tabs/Success/Success.spec.js
+++ ui.frontend/tests/unit/Components/Containers/Tabs/Success/Success.spec.js
@@ -4,10 +4,43 @@ import Texts from '@/components/micro/Texts/Texts.vue'
 import Buttons from '@/components/micro/Buttons/Buttons.vue'
 
 describe('Success wrapper', () => {
-  const wrapper = mount(Success)
-  test('Have Birthday component', () => {
+  const wrapper = mount(Success, {
+    mocks: {
+      $store: {
+        state: {
+          labelName: 'teste',
+          labelEmail: 'teste',
+          labelPhone: 'teste',
+          labelBirthday: 'teste',
+          labelNickname: 'teste',
+          labelLinkedin: 'teste',
+          labelGithub: 'teste',
+          labelCertificates: 'teste',
+          labelTeamname: 'teste',
+          labelGraduation: 'teste',
+          labelInstitution: 'teste'
+        }
+      }
+    }
+  })
+  it('fulling data', async () => {
+    await wrapper.setData({
+      nickname: 'teste',
+      phone: '54 99648 5010',
+      linkedin: 'https://google.com',
+      isCertificates: true
+    })
+  })
+
+  test('Have Texts component', () => {
     expect(wrapper.findComponent(Texts).exists()).toBe(true)
   })
+
+  test('Have all Texts with the full data', () => {
+    const texts = wrapper.findAllComponents(Texts)
+    expect(texts).toHaveLength(12)
+  })
+
   test('Have p tag', () => {
     const p = wrapper.find('p')
     expect(p.exists()).toBe(true)
